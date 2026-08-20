b08b9dc280f89380e8a92039a118e4ad21597cd0
JoaoPedroRecalcatti <joaopedrorecalcatti143@gmail.com>
2026-08-20 18:07:52 -0400

    Aplica Header no layout da area de eventos

--- frontend/src/app/eventos/layout.js
+++ frontend/src/app/eventos/layout.js
@@ -1,8 +1,13 @@
+import Header from "../components/Header";
+
 export default function EventosLayout({ children }) {
   return (
-    <div className="border-2 border-amber-500 rounded p-4 m-8">
-      <h2 className="text-2xl">Área de Eventos</h2>
-      {children}
+    <div>
+      <Header titulo={"Eventos"} />
+      <div className="border-2 border-amber-500 rounded p-4 m-8">
+        <h2 className="text-2xl">Área de Eventos</h2>
+        {children}
+      </div>
     </div>
   )
 }
